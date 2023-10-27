6f836eb25cb723b084073be214c5bf3a1b5dee4d
Angarag Gansukh <angaraggansukh@csu.fullerton.edu>
2023-10-26 18:08:11 -0700

    Set up react-routing and initial singin page

---
+++ src/root/signin.jsx
@@ -0,0 +1,34 @@
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import './signin.css'
+
+export default function Signin(){
+    return (
+        <div>
+            <section>
+                <h1>
+                    Sign In
+                </h1>
+            </section>
+            <div className='form-container'>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control type="email" placeholder='name@example.com' className='form-input'/>
+                    </Form.Group>
+                    <Form.Group style={{marginTop: '10px'}}>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type='password' placeholder='Password'/>
+                    </Form.Group>
+                </Form>
+                
+            </div>
+            <div className='signin-button'>
+                <Button>Log In</Button>
+            </div>
+            <div className='signin-button'>
+                <Button>Sign Up</Button>
+            </div>
+        </div>
+    )
+}
