000c6f061136d2c6ed3ae6cd3f14ff9a01f6f306
Angarag Gansukh <angaraggansukh@csu.fullerton.edu>
2023-11-09 15:22:22 -0800

    Sign out func

---
+++ src/routes/Settings.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Settings(){
+
+    return (
+        <div>
+            <section>
+                <h1>
+                    Settings
+                </h1>
+            </section>
+        </div>
+    )
+
+
+}
